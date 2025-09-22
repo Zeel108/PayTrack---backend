3eab641d4a7b4bd3d635a6e581c72e561f64f176
zeel <sundayy119@gmail.com>
2025-09-22 20:34:28 +0530

    initialize backend and sending hello to frontend

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/hello")
+def read_root():
+    return {"message": "Hello how are you"}
+
+@app.get("/api/first")
+def read():
+    return {"hello": "Hello world"}
