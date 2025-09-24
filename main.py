8e1fc00bf549ce980e58f48c4487b5cd9d44da48
zeel <sundayy119@gmail.com>
2025-09-24 19:45:58 +0530

    just staring from scratch

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-from fastapi import FastAPI,APIRouter
+from fastapi import FastAPI;
 
-from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI();
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-api_router = APIRouter(prefix="/api")
-
-users = [
-    {"id": 1, "name": "Alice", "email": "alice@example.com"},
-    {"id": 2, "name": "Bob", "email": "bob@example.com"},
-    {"id": 3, "name": "Charlie", "email": "charlie@example.com"},
-]
-
-@api_router.get("/users")
-def get_users():
-    return users
-
-app.include_router(api_router)
+@app.get("/")
+async def homepage():
+    return {"message": "Hello world"}
